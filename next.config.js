fab71a0f696390587f0237996b825e4b63a96295
Jacky Efendi <jacky.efendi1@gmail.com>
2020-07-12 14:40:22 +0700

    fix: pwa setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const withImages = require('next-images');
 const withOffline = require('next-offline');
 
-const config = {};
+const config = {
+  dontAutoRegisterSw: false,
+  // generateInDevMode: true,
+  generateSw: false,
+  workboxOpts: {
+    swSrc: './src/serviceWorker/base.js',
+    swDest: 'static/service-worker.js',
+  },
+};
 
-module.exports = withOffline(withImages(config));
+module.exports = withImages(withOffline(config));
